46c577db7e0ea81dac7140790ff9b3b871ebbda8
jrdespai <Ashley@elizabeth-baumbachs-macbook-4.local>
2018-07-26 20:36:10 -0700

    Adding routes

---
+++ routes.js
@@ -0,0 +1,17 @@
+var Nerd = require('./models/nerd');
+
+module.exports = function(app) {
+    
+    app.get('/app/nerds', function(req, res) {
+        Nerd.find(function(err, nerds) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(nerds);
+        });
+    });
+    
+    app.get('*', function(req, res) {
+        res.sendfile('./src/index.html');
+    });
+};
